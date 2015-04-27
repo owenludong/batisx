a2d1f12f560fd78b05e5c712c6317dd4ada21144
owen lu <owenludong.lud@alibaba-inc.com>
2015-04-27 18:19:46 +0800

    a

---
+++ batisx-war/src/main/java/org/owen/batisx/entity/DataBaseMeta.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2015 All Rights Reserved.
+ */
+package org.owen.batisx.entity;
+
+/**
+ * @author owenludong.lud
+ * @version $Id: DataBaseMeta, v 0.1  2015/4/27 18:19  owenludong.lud Exp $$
+ */
+public class DataBaseMeta {
+}
